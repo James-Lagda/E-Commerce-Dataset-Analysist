3625f3e9f131ce5c2ba9642b7c5df3d00b7312e4
James Irvin <140578157+James-Lagda@users.noreply.github.com>
2024-09-30 18:56:31 +0800

    Add files via upload

---
+++ E-commerce.sql
@@ -0,0 +1,17 @@
+Select * from `e-commerce`;
+
+select * from `e-commerce`
+where gender = 'Male';
+
+select * from `e-commerce`
+where gender = 'Female';
+
+SELECT * FROM `e-commerce`
+WHERE gender = 'Male'
+or location = 'City K'
+OR 'Annual Income' = '$70,000';
+
+SELECT * FROM `e-commerce`
+WHERE `Purchase Date 1` LIKE '%06%';
+
+
